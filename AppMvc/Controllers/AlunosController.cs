1e4e258ab4c1a908e9de13f40483d2ac3ffc6623
Rafael Lagisck <rafael.lagisck@exemplo.com>
2023-08-29 17:54:15 -0300

    Action Filters

--- AppMvc/Controllers/AlunosController.cs
+++ AppMvc/Controllers/AlunosController.cs
@@ -11,11 +11,13 @@ using AppMvc.Models;
 
 namespace AppMvc.Controllers
 {
+    [Authorize]
     public class AlunosController : Controller
     {
         private readonly ApplicationDbContext db = new ApplicationDbContext();
 
         [HttpGet]
+        [AllowAnonymous]
         [Route("listar-alunos")]
         public async Task<ActionResult> Index()
         {
